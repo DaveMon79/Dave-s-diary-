b057b74d0848d680d96eb9b2feafafd8f439024c
DMon <davemonaghan@gmx.com>
2022-07-31 12:42:30 +0100

    remaining functions working

--- script.js
+++ script.js
@@ -1,49 +1,71 @@
 
-var displaytDate = moment().format("dddd Do MMMM YYYY");
-$("#currentDay").text(displaytDate);
-
-var displaytime = moment().format("LTS");
-$("#currenttime").text(displaytime);
-
-
-
 $(document).ready(function () {
+
     $(".saveBtn").click(function () {
         var userText = $(this).siblings(".description").val();
         var hour = $(this).parent().attr('id');
-        $(this).siblings(".description")
         localStorage.setItem(hour, userText);
+
+      
+        setTimeout(function () {
+            $('#show-hide').addClass('storage-alert');
+            }, 3000);
+            
+            $('#show-hide').removeClass('storage-alert');
 
     });
 
+
+    function timeTracker() {
+
+        var currentTime = moment().format("H");
+      
+        $(".row").each(function () {
+            var rowTime = parseInt($(this).attr("id"));
+
+            if (rowTime < currentTime) {
+                 $(this).addClass("past");
+            }
+            else if (rowTime == currentTime) {
+                $(this).addClass("present");
+            }
+            else {
+                $(this).addClass("future");
+            }
+
+        });
+    }
+
+
+   
     
+    $('#8 .description').val(localStorage.getItem('8'));
+    $('#9 .description').val(localStorage.getItem('9'));
+    $('#10 .description').val(localStorage.getItem('10'));
+    $('#11 .description').val(localStorage.getItem('11'));
+    $('#12.description').val(localStorage.getItem('12'));
+    $('#13 .description').val(localStorage.getItem('13'));
+    $('#14 .description').val(localStorage.getItem('14'));
+    $('#15 .description').val(localStorage.getItem('15'));
+    $('#16 .description').val(localStorage.getItem('16'));
+    $('#17 .description').val(localStorage.getItem('17'));
+
+
+    var displaytDate = moment().format("dddd Do MMMM YYYY");
+    $("#currentDay").text(displaytDate);
+
+    function displayTime() {
+        var rightNow = moment().format('H:mm:ss a');
+        $("#currenttime").text(rightNow);
+
+    }
+
+    setInterval(displayTime, 1000) + timeTracker();
+
+ 
+
 });
 
 
 
-
-
-function timeTracker() {
- 
-    var currentTime = moment().hour();
-
-    // loop over time blocks
-    $(".time-block").each(function () {
-        var blockTime = $(this).attr("id").val;
-        console.log(blockTime);
-
-        if (blockTime < currentTime) {
-            $(this).addClass("past");
-        }
-        else if (blockTime === currentTime) {
-            $(this).removeClass(".past");
-            $(this).addClass(".present");
-        }
-        else if (blockTime > currentTime) 
-            $(this).removeClass(".present");
-            $(this).addClass(".future");
-        
-    }
-);
-}
 
